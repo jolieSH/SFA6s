52b36f0cd15432e31480b71c5f3b7c2e5e2b18d0
ebest <gem install selenium-webdriver>
2017-12-21 18:09:57 +0800

    m

--- all_case.py
+++ all_case.py
@@ -1,13 +1,12 @@
 #coding=utf-8
 import unittest
-import os
 
 def createsuite():
     testunit=unittest.TestSuite()
     #定义测试查找的目录
     dir='D:\\SFA6s\\SFA6s'
     #定义discover方法的参数
-    discover=unittest.defaultTestLoader.discover(dir,pattern='*.py',top_level_dir=None)
+    discover=unittest.defaultTestLoader.discover(dir,pattern='login*.py',top_level_dir=None)
     #discover筛选出的用例，循环添加到测试套件中
     for test_suite in discover:
         for test_case in test_suite:
@@ -15,7 +14,7 @@ def createsuite():
             print testunit
     return testunit
 
-if __name__ ==  '__main__':
+if __name__ == '__main__':
     runner=unittest.TextTestRunner()
     runner.run(testunit)
 
