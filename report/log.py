239d38a16136daa5eb8c177fde0ee2ff96b315e5
ebest <gem install selenium-webdriver>
2017-12-22 10:04:56 +0800

    m

--- report/log.py
+++ report/log.py
@@ -1,5 +1,4 @@
 #coding=utf-8
-import  os
-os.system('cd D:\SFA6s\SFA6s\report ')
-os.system('D:')
-os.system('python log.py >> ./log.txt 2>&1 ')
+import os
+os.chdir('D:\\SFA6s\\SFA6s\\report ')
+os.system('python all_case.py >> ./log.txt 2>&1 ')
